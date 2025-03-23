9cc7b5c78fc825c0ef4c6b2ebba4ea06e448a626
yashpratap <pratapyashkulshrestha@gmail.com>
2025-03-23 22:48:57 +0530

    all: updates files

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,72 +1,87 @@
 function ColorMyPencils(color)
-  color = color or "catppuccin-mocha"
-  vim.cmd.colorscheme(color)
+	color = color or "rose-pine-moon"
+	vim.cmd.colorscheme(color)
 
-  -- If transparent background is not supported by the colorscheme
-  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- If transparent background is not supported by the colorscheme
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = { transparent_background = true },
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+		},
+	},
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    opts = {
-      transparent_background = "true",
-    },
-    config = function()
-      ColorMyPencils()
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		opts = {
+			style = "night",
+			transparent = true,
+		},
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				transparent = true,
+			})
+		end,
+	},
 
-  {
-    "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = false,
-        bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-    end,
-  },
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true, -- add neovim terminal colors
+				undercurl = true,
+				underline = false,
+				bold = true,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false,
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_intend_guides = false,
+				inverse = true, -- invert background for search, diffs, statuslines and errors
+				contrast = "", -- can be "hard", "soft" or empty string
+				palette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = false,
+			})
+		end,
+	},
 
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        disable_background = true,
-        styles = {
-          italic = false,
-        },
-      })
-    end,
-  },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		opts = {
+			disable_background = true,
+			styles = {
+				italic = false,
+			},
+		},
+	},
+
+	{
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.moonflyTransparent = true
+			ColorMyPencils("moonfly")
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -4,4 +4,23 @@ return {
 
 	-- copilot
 	"github/copilot.vim",
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		opts = {
+			save_path = "~/Pictures/CodeSnap/",
+			watermark = "",
+			bg_x_padding = 80,
+			bg_y_padding = 60,
+			bg_theme = "grape",
+		},
+	},
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
