8c4635a5e499c0d9d5f8aa164229a99db2569cc7
yashpratap <pratapyashkulshrestha@gmail.com>
2025-05-01 15:26:39 +0530

    reinitialize

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,36 +1,36 @@
 return { -- Autoformat
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            "<leader>cf",
-            function()
-                require("conform").format({ async = true, lsp_format = "fallback" })
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    opts = {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-            local disable_filetypes = { c = true, cpp = true, html = true }
-            local lsp_format_opt
-            if disable_filetypes[vim.bo[bufnr].filetype] then
-                lsp_format_opt = "never"
-            else
-                lsp_format_opt = "fallback"
-            end
-            return {
-                timeout_ms = 500,
-                lsp_format = lsp_format_opt,
-            }
-        end,
-        formatters_by_ft = {
-            lua = { "stylua" },
-            javascript = { "prettierd", "prettier", stop_after_first = true },
-            typescript = { "prettierd", "prettier", stop_after_first = true },
-        },
-    },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		notify_on_error = false,
+		format_on_save = function(bufnr)
+			local disable_filetypes = { c = true, cpp = true, html = true }
+			local lsp_format_opt
+			if disable_filetypes[vim.bo[bufnr].filetype] then
+				lsp_format_opt = "never"
+			else
+				lsp_format_opt = "fallback"
+			end
+			return {
+				timeout_ms = 500,
+				lsp_format = lsp_format_opt,
+			}
+		end,
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+		},
+	},
 }
